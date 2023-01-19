a2699a2df8baedc3a24a3a1e19863e12d41edaa3
Janhaviprabhu <januprabhu26@gmail.com>
2023-01-19 15:49:43 +0530

    check

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.set('strictQuery', true)
+
 const connection = mongoose.connect("mongodb+srv://jahn:12345@cluster0.xmolpcc.mongodb.net/test?retryWrites=true&w=majority")
 
 module.exports = {
