675975abeac85089d8b8ef03b8fb8b49fed7ffdf
Janhaviprabhu <januprabhu26@gmail.com>
2023-01-19 16:09:20 +0530

    check1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-
-const connection = mongoose.connect("mongodb+srv://jahn:12345@cluster0.xmolpcc.mongodb.net/test?retryWrites=true&w=majority")
+const DB_URL="mongodb+srv://jahn:12345@cluster0.xmolpcc.mongodb.net/test?retryWrites=true&w=majority"
+const connection = mongoose.connect(DB_URL)
 
 module.exports = {
     connection
